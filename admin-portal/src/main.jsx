3c0134b4cbda794f3566e55bdb14571477638d74
Badmus Qudus Ayomide <badmusqudusayomide@gmail.com>
2026-07-12 19:28:50 +0100

    feat: add a blank line for improved readability in main.jsx

--- admin-portal/src/main.jsx
+++ admin-portal/src/main.jsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App';
 // import './styles/global'
 import './styles/global.css';
+  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
